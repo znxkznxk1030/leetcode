ca8f327c1000aade1e1ca5094f7d8afc3fbead7f
Arthur 金 <znxkznxk1030@yonsei.ac.kr>
2023-01-27 17:53:21 +0900

    Time: 572 ms (5.14%), Space: 145.8 MB (5.14%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        Set<String> wordSet = new HashSet<>();
+        List<String> result = new ArrayList<>();
+
+        Arrays.sort(words, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.length()- s2.length();
+            }
+        });
+
+        for (String word: words) {
+            // System.out.println(word);
+            List<Integer> pivotList = new ArrayList<>();
+            pivotList.add(0);
+            
+            for(int i = 0; i <= word.length(); i++) {
+                for (int pivot: pivotList.stream().collect(Collectors.toList())) {
+                    String tmp = word.substring(pivot, i);
+                    if (wordSet.contains(tmp)) {
+                        pivotList.add(i);
+                        if (i == word.length()) {
+                            System.out.println(word);
+                            result.add(word);
+                        }
+                    }
+                }
+            }
+
+            wordSet.add(word);
+        }
+
+        result = result.stream().distinct().collect(Collectors.toList());
+
+        return result;
+    }
+}
