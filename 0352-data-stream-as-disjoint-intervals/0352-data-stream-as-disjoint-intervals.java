f6a6e176213ee11f3af0f2c04ef68b3e1e67f397
Arthur 金 <znxkznxk1030@yonsei.ac.kr>
2023-01-28 21:53:23 +0900

    Time: 295 ms (5.04%), Space: 74.9 MB (15.13%) - LeetHub

---
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.java
@@ -0,0 +1,79 @@
+class SummaryRanges {
+
+    List<int[]> data;
+    public SummaryRanges() {
+        data = new LinkedList<>();
+    }
+    
+    public void addNum(int value) {
+        if (data.isEmpty()) {
+            int[] num = {value, value};
+            data.add(num);
+            return;
+        }
+        
+        int low = 0, high = data.size();
+        int mid = low + (high - low) / 2;
+        while (low < high) {
+            mid = low + (high - low) / 2;
+            int[] curr = data.get(mid);
+            System.out.println("mid - " + curr[0] + " - " + curr[1]);
+            
+            if (curr[0] > value) {
+                high = mid ;
+            } else if (curr[0] <= value) {
+                low = mid + 1;
+            }
+        }
+        
+        System.out.println(low);
+        int[] num = {value, value};
+        data.add(low, num);
+        int index = low;
+        
+        int[] prev = index >= 1?data.get(index - 1):null;
+        int[] post = data.size() > index + 1?data.get(index + 1):null;
+        
+        System.out.println("prev - " + prev + " post - " + post);
+        
+        while((prev != null && prev[1] + 1 >= num[0]) || (post != null && post[0] <= num[1] + 1)) {
+            if (prev != null && prev[1] + 1 >= num[0]) {
+                prev[1] = Math.max(num[1], prev[1]);
+                data.remove(index);
+                num = prev;
+                index = index - 1;
+                
+                System.out.println("prev - index : " + index);
+                System.out.println("prev : " + prev[0] + " - " + prev[1]);
+            }
+            
+            if (post != null && post[0] <= num[1] + 1) {
+                post[0] = Math.min(num[0], post[0]);
+                System.out.println("post - index : " + index);
+                System.out.println("post : " + post[0] + " - " + post[1]);
+                data.remove(index);
+                num = post;
+            }
+            
+            prev = index >= 1?data.get(index - 1):null;
+            post = data.size() > index + 1?data.get(index + 1):null;
+        }
+    }
+    
+    public int[][] getIntervals() {
+        int[][] result = new int[data.size()][2];
+        
+        for (int i = 0 ; i < data.size(); i++) {
+            result[i] = data.get(i);
+        }
+        
+        return result;
+    }
+}
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * SummaryRanges obj = new SummaryRanges();
+ * obj.addNum(value);
+ * int[][] param_2 = obj.getIntervals();
+ */
