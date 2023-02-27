992aa081176e6402b94a37fa9a30dac07b37f148
Arthur 金 <znxkznxk1030@yonsei.ac.kr>
2023-02-27 18:23:31 +0900

    Time: 136 ms (57.81%), Space: 33 MB (47.66%) - LeetHub

---
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.kt
@@ -0,0 +1,25 @@
+/** 
+ * The API guess is defined in the parent class.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * fun guess(num:Int):Int {}
+ */
+
+class Solution:GuessGame() {
+    override fun guessNumber(n:Int):Int {
+        var left = 0
+        var right = n
+
+        while (left <= right) {
+            val mid = left + (right - left) / 2
+            
+            if (guess(mid) == 0) return mid
+            if (guess(mid) < 0) right = mid - 1
+            else left = mid + 1
+        }
+
+        return -1
+    }
+}
