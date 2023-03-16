be0efed73c40ab50b759f116d879d4550ff754ca
Arthur 金 <znxkznxk1030@yonsei.ac.kr>
2023-03-16 12:46:45 +0900

    Time: 222 ms (53.13%), Space: 69.8 MB (18.75%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.kt
@@ -0,0 +1,30 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun buildTree(inorder: IntArray, postorder: IntArray): TreeNode? {
+        if (postorder.size == 1) return TreeNode(postorder[postorder.lastIndex])
+        if (postorder.size == 0) return null
+        
+        val root = TreeNode(postorder[postorder.lastIndex])
+        val idx = inorder.indexOf(postorder[postorder.lastIndex])
+        
+        if (idx >= 0) {
+            // println(Arrays.toString(inorder.copyOfRange(0, idx)))
+            // println(Arrays.toString(postorder.copyOfRange(0, idx)))
+            // println(Arrays.toString(inorder.copyOfRange(idx + 1, inorder.size)))
+            // println(Arrays.toString(postorder.copyOfRange(idx, postorder.size - 1)))
+            root.left = buildTree(inorder.copyOfRange(0, idx), postorder.copyOfRange(0, idx))
+            root.right = buildTree(inorder.copyOfRange(idx + 1, inorder.size), postorder.copyOfRange(idx, postorder.size - 1))
+        }
+        
+        return root
+    }
+}
