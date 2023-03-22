c76a6b48b2da82df7cdc889085928e2486460784
Arthur 金 <znxkznxk1030@yonsei.ac.kr>
2023-03-22 11:25:47 +0000

    Time: 1397 ms (100.00%), Space: 131.3 MB (100.00%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.kt
@@ -0,0 +1,31 @@
+class Solution {
+    fun minScore(n: Int, roads: Array<IntArray>): Int {
+        val graph: MutableMap<Int, MutableList<Pair<Int, Int>>> = mutableMapOf()
+        
+        for (road in roads) {
+            graph.computeIfAbsent(road[0]){ mutableListOf() }.add(Pair(road[1], road[2]))
+            graph.computeIfAbsent(road[1]){ mutableListOf() }.add(Pair(road[0], road[2]))
+        }
+        
+        println("${graph}")
+        
+        var result = Int.MAX_VALUE
+        
+        val q = LinkedList<Int>()
+        val visit = BooleanArray(n + 1){false}
+        q.offer(1)
+        while(!q.isEmpty()) {
+            val curr = q.poll()
+            if (visit[curr]) continue
+            visit[curr] = true
+            
+            for(edge in graph.getOrDefault(curr, mutableListOf())) {
+                if (visit[edge.first] == true) continue
+                q.offer(edge.first)
+                result = Math.min(edge.second, result)
+            }
+        }
+        
+        return result
+    }
+}
