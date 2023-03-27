6c0f4a82f6967f826e859070808c58eecba7b2b5
Arthur 金 <znxkznxk1030@yonsei.ac.kr>
2023-03-27 20:22:12 +0900

    Time: 6 ms (38.78%), Space: 48.9 MB (32.42%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -1,31 +1,13 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        int length = nums.length;
-        int[] dp1 = new int[length];
-        int[][] dp = new int[nums.length + 1][3];
-
-        for (int i = 0; i < length; i++) {
-            dp1[i] = -3999999;
-            for (int j = 0; j < 3; j++) {
-                dp[i][j] = -3999999;
-            }
+        int curMax = 0, curMin = 0, sum = 0, maxSum = nums[0], minSum = nums[0];
+        for (int num : nums) {
+            curMax = Math.max(curMax, 0) + num;
+            maxSum = Math.max(maxSum, curMax);
+            curMin = Math.min(curMin, 0) + num;
+            minSum = Math.min(minSum, curMin);
+            sum += num;  
         }
-
-        dp1[0] = nums[0];
-        dp[0][0] = nums[0];
-        int result = nums[0];
-
-        for (int i = 1; i < length; i++) {
-            dp1[i] = Math.max(dp1[i - 1] + nums[i], nums[i]);
-            result = Math.max(result, dp1[i]);
-
-            dp[i][0] = dp[i - 1][0] + nums[i];
-            dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1]);
-            dp[i][2] = Math.max(dp[i - 1][1], dp[i - 1][2]) + nums[i];
-        }
-
-        result = Math.max(result, dp[length - 1][2]);
-
-        return result;
+        return sum == minSum ? maxSum : Math.max(maxSum, sum - minSum);
     }
 }
