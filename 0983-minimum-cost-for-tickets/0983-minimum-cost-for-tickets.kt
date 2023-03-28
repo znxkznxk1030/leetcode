c9550f6ecc2f8c27da0c0b72fd0c998491ac1ecf
Arthur 金 <znxkznxk1030@yonsei.ac.kr>
2023-03-28 16:13:35 +0900

    Time: 150 ms (100.00%), Space: 35.7 MB (78.13%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.kt
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.kt
@@ -1,27 +1,26 @@
 class Solution {
     var costs = IntArray(3)
     var dp = IntArray(366){ -1 }
-    val dayset = HashSet<Int>()
+    var dayset = HashSet<Int>()
     
     fun mincostTickets(days: IntArray, costs: IntArray): Int {
         this.costs = costs
+        
         days.forEach{ dayset.add(it) }
+        
         return dfs(1)
     }
     
-    fun dfs(day: Int): Int {
-        if (day > 365) return 0
-        if (dp[day] > 0) return dp[day]
+    fun dfs(i: Int): Int {
+        if (i > 365) return 0
+        if (dp[i] > 0) return dp[i]
         
-        var result: Int
-        
-        if (dayset.contains(day)) {
-            result = Math.min(dfs(day + 1) + costs[0], Math.min(dfs(day + 7) + costs[1], dfs(day + 30) + costs[2]))
+        if (dayset.contains(i)) {
+            dp[i] = Math.min(dfs(i + 1) + costs[0], Math.min(dfs(i + 7) + costs[1], dfs(i + 30) + costs[2]))
         } else {
-            result = dfs(day + 1)
+            dp[i] = dfs(i + 1)
         }
         
-        dp[day] = result
-        return result
+        return dp[i]
     }
 }
