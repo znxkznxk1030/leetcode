5cbcd34e4ba3017a70ae032c0ff097ac7d002b17
Arthur 金 <znxkznxk1030@yonsei.ac.kr>
2023-03-28 17:58:05 +0900

    Time: 155 ms (100.00%), Space: 34.4 MB (100.00%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.kt
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.kt
@@ -1,26 +1,25 @@
 class Solution {
-    var costs = IntArray(3)
-    var dp = IntArray(366){ -1 }
-    var dayset = HashSet<Int>()
-    
     fun mincostTickets(days: IntArray, costs: IntArray): Int {
-        this.costs = costs
+        val durations = intArrayOf(1, 7, 30)
+        val dp = IntArray(days.size){ Int.MAX_VALUE }
         
-        days.forEach{ dayset.add(it) }
-        
-        return dfs(1)
-    }
-    
-    fun dfs(i: Int): Int {
-        if (i > 365) return 0
-        if (dp[i] > 0) return dp[i]
-        
-        if (dayset.contains(i)) {
-            dp[i] = Math.min(dfs(i + 1) + costs[0], Math.min(dfs(i + 7) + costs[1], dfs(i + 30) + costs[2]))
-        } else {
-            dp[i] = dfs(i + 1)
+        fun dfs(i: Int): Int {
+            if (i >= days.size) return 0
+            if (dp[i] < Int.MAX_VALUE) return dp[i]
+            
+            for (j in costs.indices) {
+                val cost = costs[j]
+                val duration = durations[j]
+                var k = 0
+                
+                while(i + k < days.size && days[i + k] - days[i] < duration) k++
+                
+                dp[i] = Math.min(dp[i], dfs(i + k) + cost)
+            }
+            
+            return dp[i]
         }
         
-        return dp[i]
+        return dfs(0)
     }
 }
