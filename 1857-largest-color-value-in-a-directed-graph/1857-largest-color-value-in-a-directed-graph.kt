d684552e74fd90fa76ce0660bae5e84097d96d73
Arthur 金 <znxkznxk1030@yonsei.ac.kr>
2023-04-10 15:07:46 +0000

    Time: 1783 ms (33.33%), Space: 238.4 MB (33.33%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.kt
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.kt
@@ -1,47 +1,50 @@
 class Solution {
     fun largestPathValue(colors: String, edges: Array<IntArray>): Int {
         val n = colors.length
-        val adj = HashMap<Int, MutableList<Int>>()
+        val adj = Array(n){ mutableListOf<Int>() }
         val indegree = IntArray(n){ 0 }
         
         for (edge in edges) {
-            adj.computeIfAbsent(edge[0]){ mutableListOf<Int>() }.add(edge[1])
-            indegree[edge[1]]++
+            val u = edge[0]
+            val v = edge[1]
+            adj[u].add(v)
+            indegree[v]++
         }
         
-        val count = Array(n){ IntArray(26) }
         val q = LinkedList<Int>()
-        
-        for (i in 0 until n) {
+        for (i in indegree.indices) {
             if (indegree[i] == 0) {
                 q.offer(i)
             }
         }
         
-        var result = 1
-        var nodesSeen = 0
+        var nodeSeen = 0
+        var result = 0
+        val count = Array(n){ HashMap<Char, Int>() }
         
         while(!q.isEmpty()) {
             val curr = q.poll()
-            result = Math.max(result, ++count[curr][colors[curr] - 'a'])
-            nodesSeen++
+            val color = colors[curr]
+            nodeSeen++
             
-            if (!adj.containsKey(curr)) continue
+            val neighbors = adj[curr]
+            val countOfColor = count[curr].getOrDefault(color, 0) + 1
+            count[curr].put(color, countOfColor)
             
-            for (neighbor in adj.get(curr)!!) {
-                for (i in 0..25) {
-                    count[neighbor][i] = Math.max(count[neighbor][i], count[curr][i])
-                }
-                
+            for (neighbor in neighbors) {
                 indegree[neighbor]--;
                 if (indegree[neighbor] == 0) {
                     q.offer(neighbor)
                 }
+                
+                for (ch in 'a'..'z') {
+                    count[neighbor].put(ch, Math.max(count[neighbor].getOrDefault(ch, 0), count[curr].getOrDefault(ch, 0)))
+                }
             }
             
+            result = Math.max(countOfColor, result)
         }
         
-        
-        return if ( nodesSeen < n ) -1 else result
+        return if (nodeSeen < n) -1 else result
     }
 }
