20cf2d8b36bb3ee2814b9189e942008612911290
Arthur 金 <znxkznxk1030@yonsei.ac.kr>
2023-04-13 11:00:08 +0900

    Time: 180 ms (80.00%), Space: 36.9 MB (60.00%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun validateStackSequences(pushed: IntArray, popped: IntArray): Boolean {
+        val stack = Stack<Int>()
+        var j = 0
+        for (i in pushed) {
+            stack.push(i)
+            
+            while(!stack.isEmpty() && stack.peek() == popped[j]){
+                stack.pop()
+                j++
+            }
+        }
+        
+        while(!stack.isEmpty() && stack.peek() == popped[j]){
+            stack.pop()
+            j++
+        }
+        
+        return j == popped.size && stack.isEmpty()
+    }
+}
