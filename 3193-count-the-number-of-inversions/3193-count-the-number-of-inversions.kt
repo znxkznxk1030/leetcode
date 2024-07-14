51b59317edfbe1fdad000d31e4e0cfe2658e5c59
Arthur 金 <znxkznxk1030@yonsei.ac.kr>
2024-07-14 15:11:52 +0900

    Time: 1010 ms (21.74%), Space: 57.3 MB (73.91%) - LeetHub

--- 3193-count-the-number-of-inversions/3193-count-the-number-of-inversions.kt
+++ 3193-count-the-number-of-inversions/3193-count-the-number-of-inversions.kt
@@ -1,43 +1,37 @@
 class Solution {
-    val MOD = 1_000_000_007
+    val mod = 1_000_000_007
+    
     fun numberOfPermutations(n: Int, requirements: Array<IntArray>): Int {
-        val map = HashMap<Int, Int>()
+        val rmap = HashMap<Int, Int>()
         for (req in requirements) {
-            map.put(req[0] + 1, req[1])
+            rmap[req[0] + 1] = req[1]
         }
         
-        val dp = Array(n + 1){ IntArray( 401 ) }
+        val dp = Array(n + 1){ IntArray(401) }
         dp[0][0] = 1
         
-        for (l in 1 .. n) {
+        for(l in 1 .. n) {
             for (inv in 0 until 401) {
-                for (pos in 0 until l) {
-                    val pInv = inv - pos
-                    if (pInv >= 0) {
-                        dp[l][inv] += dp[l - 1][pInv]
-                        dp[l][inv] %= MOD
-                    }
+                for(pos in 0 until l) {
+                    if (inv - pos < 0) continue
+                    dp[l][inv] += dp[l - 1][inv - pos]
+                    dp[l][inv] %= mod
                 }
             }
             
-            if (map.containsKey(l)) {
-                val tInv = map[l]
-                
+            if(rmap.containsKey(l)) {
                 for (inv in 0 until 401) {
-                    if (inv != tInv) {
-                        dp[l][inv] = 0
-                    }
+                    if(inv == rmap[l]) continue
+                    dp[l][inv] = 0
                 }
             }
         }
         
-        
-        
         var result = 0
         
-        for(m in 0 until 401) {
+        for (m in 0 until 401) {
             result += dp[n][m]
-            result %= MOD
+            result %= mod
         }
         
         return result
