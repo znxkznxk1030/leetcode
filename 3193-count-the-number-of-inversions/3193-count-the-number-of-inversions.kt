a7b8e17ba707c62149a1abdbecdcab1a2be4576f
Arthur 金 <znxkznxk1030@yonsei.ac.kr>
2024-07-14 17:38:54 +0900

    Time: 746 ms (73.91%), Space: 49.3 MB (82.61%) - LeetHub

--- 3193-count-the-number-of-inversions/3193-count-the-number-of-inversions.kt
+++ 3193-count-the-number-of-inversions/3193-count-the-number-of-inversions.kt
@@ -2,15 +2,17 @@ class Solution {
     val MOD = 1_000_000_007
     fun numberOfPermutations(n: Int, requirements: Array<IntArray>): Int {
         val rmap = HashMap<Int, Int>()
+        var max = 0
         for (req in requirements) {
             rmap[req[0] + 1] = req[1]
+            max = maxOf(max, req[1])
         }
         
-        val dp = Array(n + 1){ IntArray(401) }
+        val dp = Array(n + 1){ IntArray(max + 1) }
         dp[0][0] = 1
         
         for (i in 1 .. n) {
-            for (inv in 0 until 401) {
+            for (inv in 0 .. max) {
                 for (pos in 0 until i) {
                     if(inv - pos < 0) continue
                     dp[i][inv] += dp[i - 1][inv - pos]
@@ -19,7 +21,7 @@ class Solution {
             }
             
             if (!rmap.containsKey(i)) continue
-            for (inv in 0 until 401) {
+            for (inv in 0 .. max) {
                 if(rmap[i] == inv) continue
                 dp[i][inv] = 0
             }
@@ -27,7 +29,7 @@ class Solution {
         
         var result = 0
         
-        for (i in 0 until 401) {
+        for (i in 0 .. max) {
             result += dp[n][i]
             result %= MOD
         }
